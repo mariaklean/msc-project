ee7ea8032c9c7844d8bfcdff9be9e39a56171861
Maria <makleanthous@gmail.com>
2021-11-15 10:41:48 +0200

    products screen

---
+++ backend/data.js
@@ -0,0 +1,79 @@
+/*simple data fro practise*/
+
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'kebab',
+            image:'/images/p1.jpg',
+            price:15,
+            countInStock:3,
+            type: 'beef',
+            rating:4,
+            numReviews:60,
+            description: 'best homemade pork',
+
+        },
+        {
+            _id:'2',
+            name:'salad',
+            image:'/images/p2.jpg',
+            price:15,
+            countInStock:3,
+            type: 'vegan',
+            rating:4,
+            numReviews:60,
+            description: 'best homemade salad',
+
+        },
+        {
+            _id:'3',
+            name:'chicken',
+            image:'/images/p3.jpg',
+            price:15,
+            countInStock:0,
+            type: 'chicken',
+            rating:4,
+            numReviews:60,
+            description: 'best homemade chicken',
+
+        },
+        {
+            _id:'4',
+            name:'pizza',
+            image:'/images/p4.jpg',
+            price:15,
+            countInStock:13,
+            type: 'italian',
+            rating:4,
+            numReviews:60,
+            description: 'best homemade pizza',
+
+        },
+        {
+            _id:'5',
+            name:'puncakes',
+            image:'/images/p5.jpg',
+            price:14,
+            type: 'puncakes',
+            rating:3,
+            numReviews:60,
+            description: 'best homemade puncakes',
+
+        },
+        {
+            _id:'6',
+            name:'fries',
+            image:'/images/p6.jpg',
+            price:15,
+            countInStock:3,
+            type: 'vegie',
+            rating:2,
+            numReviews:20,
+            description: 'best homemade Fries',
+
+        },
+
+    ],
+};
+export default data;
