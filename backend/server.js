9aef46f3cbeff5c909c78326698ca013e706a75a
Maria <makleanthous@gmail.com>
2021-12-02 17:25:52 +0200

    place order

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,22 @@ import data from './data.js';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import dotenv from 'dotenv';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
 const app = express();
 
 
-//resolve the undedifies email on postman
+/*resolve the undedifies email on postman */
 app.use(express.json());
 app.use(express.urlencoded({extend:true}));
-
 mongoose.connect(process.env.MONGODB_URL ||  'mongodb://localhost/project2021');
 
 
 app.use("/api/users", userRouter);
 app.use('/api/products',productRouter);
+app.use('/api/orders', orderRouter);
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
