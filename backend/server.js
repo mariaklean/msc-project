5ee7951d9fb685325cde7ca40376b59846693772
Maria <makleanthous@gmail.com>
2021-12-06 17:30:59 +0200

    fixing

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,8 @@ app.use((err,req,res,next)=>{
   res.status(500).send({message:err.message})
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
