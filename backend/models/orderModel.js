5830afd436316ed05a6633e2a7f35f2786aadc09
Maria <makleanthous@gmail.com>
2021-12-06 15:26:38 +0200

    order list done

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -21,6 +21,8 @@ const orderSchema = new mongoose.Schema(
       city: { type: String, required: true },
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
+      notes: { type: String, required:false },
+
     },
     paymentMethod: { type: String, required: true },
     itemsPrice: { type: Number, required: true },
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -50,6 +50,16 @@ userRouter.post('/register', expressAsyncHandler(async(req,res)=>{
         }
     );
 
+}));
+
+userRouter.get('/:id', expressAsyncHandler(async(req,res)=>
+{
+    const user =await User.findById(req.params.id);
+    if (user){
+        res.send(user);
+    }else {
+        res.status(404).send({message: 'user not excist'});
+    }
 }))
 
 export default userRouter;
