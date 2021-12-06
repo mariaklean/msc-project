948384e8df2dcca2f4862c1c43eb0037c4924b79
Maria <makleanthous@gmail.com>
2021-12-06 17:36:50 +0200

    fixing things

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,7 @@ app.get('/', (req, res) => {
 app.use((err,req,res,next)=>{
   res.status(500).send({message:err.message})
 });
-
-var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
